49b5a29cbe28d528697fa68287693056c22752b7
divyansh747 <divyansh.1999.dr@gmail.com>
2021-01-31 00:04:16 +0530

    fix bug: overlapping scores due to low network bandwidth

--- crickscorebot.py
+++ crickscorebot.py
@@ -7,42 +7,46 @@ import telegram
 token = "ADD TOKEN"
 bot = telegram.Bot(token)
 scorelist = [0]*100
+prevdata = ""
 
 while True:
 
     count = 0
     response = requests.get('https://hs-consumer-api.espncricinfo.com/v1/pages/matches/live?')
+    time.sleep(5)
     data = json.loads(response.text)
 
-    for liveInning in data['content']['matches']:
-        if liveInning['liveInning'] != None:
-            msg = ""
-            msg = msg + "### "+ liveInning['slug']  +" ###" + "\n"
-            msg = msg + liveInning['state'] + "\n"
-            msg = msg + liveInning['statusText'] + "\n"
-
-            for isLive in liveInning['teams']:
+    if prevdata != data:
+        prevdata = data
+        for liveInning in data['content']['matches']:
+            if liveInning['liveInning'] != None:
+                msg = ""
+                msg = msg + "### "+ liveInning['slug']  +" ###" + "\n"
+                msg = msg + liveInning['state'] + "\n"
+                msg = msg + liveInning['statusText'] + "\n"
                 flag = False
 
-                if isLive['isLive'] == False:
-                    if isLive['score'] == None:
-                        msg = msg + "Bowling..." + "\n"
-                    else:
-                        msg = msg + isLive['team']['name']+" scored: "+isLive['score'] + "\n"
-                if isLive['isLive'] == True:
-                    if scorelist[count] == 0:
-                        scorelist.insert(count, isLive['score'])
-                        flag = True
-                    elif scorelist[count] != isLive['score']:
-                        flag = True
-                        scorelist[count] = isLive['score']
-                    else:
-                        flag = False
+                for isLive in liveInning['teams']:
 
-                    msg = msg + isLive['team']['name']+" current score: "+isLive['score'] + "\n"
-                    msg = msg + "Score Info: "+isLive['scoreInfo'] + "\n"
+                    if isLive['isLive'] == False:
+                        if isLive['score'] == None:
+                            msg = msg + "Bowling..." + "\n"
+                        else:
+                            msg = msg + isLive['team']['name']+" scored: "+isLive['score'] + "\n"
+                    if isLive['isLive'] == True:
+                        if scorelist[count] == 0:
+                            scorelist.insert(count, isLive['score'])
+                            flag = True
+                        elif scorelist[count] != isLive['score']:
+                            flag = True
+                            scorelist[count] = isLive['score']
+                        else:
+                            flag = False
+
+                        msg = msg + isLive['team']['name']+" current score: "+isLive['score'] + "\n"
+                        msg = msg + "Score Info: "+isLive['scoreInfo'] + "\n"
 
                 if flag == True:
-                    output = bot.send_message(chat_id="ADD CHAT ID", text=msg)
+                   output = bot.send_message(chat_id="ADD CHAT ID", text=msg)
 
-            count = count+1
+                count = count+1
